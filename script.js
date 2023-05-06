3163201f311c78e992d6ef963fd158ba20e266cc
carlos <junior.pessoaagradavel@gmail.com>
2023-05-05 21:02:00 -0300

    resto da aula

--- script.js
+++ script.js
@@ -76,7 +76,7 @@ function CompararNumeros(num1,num2){
     }  
  */
 
-    function CarcularIdade(anoNasc){
+   /*  function CarcularIdade(anoNasc){
         let anoAtual= 2023;
         let idade = anoAtual- anoNasc;
         let mensagem = "";
@@ -92,4 +92,76 @@ function CompararNumeros(num1,num2){
         }
         return mensagem
 
+    } */
+/*  let ensinoMedio= prompt('voce concluiu o ensino medio?')
+ let idade= Number(prompt('qual sua idade?'))
+ let outraFacul= prompt('voce esta cursando outra faculdade?')
+
+ 
+
+    function verificarFacul(ensinoMedio, idade , outraFacul){
+
+        let mensgem=''
+        if(ensinoMedio.toUpperCase() ==="SIM" && idade>=18 && outraFacul.toUpperCase()==='NAO'){
+            mensgem='seja bem vindo'
+
+        }else{
+            mensgem='cumpra todas as condiçoes para entrar nesta instituiçao'
+        }
+        return mensgem
+
     }
+
+ let resultado = verificarFacul(ensinoMedio, idade , outraFacul)
+
+ console.log(resultado) */
+
+
+ /* let pais ='croacia'
+
+ switch(pais){
+    case 'brasil':
+        console.log ('brasileiro') 
+        break;
+    case 'lichtein':
+        console.log( 'lichteinense')
+        break;
+    case 'croacia':
+            console.log( 'croata')
+            break;
+    default:
+        console.log( 'nacionalidade nao encontrada')
+        break
+    
+
+ } */
+
+ let algarismo1 = Number(prompt('Numero 1'))
+ let algarismo2 = Number(prompt('Numero 2'))
+
+
+let opçao = Number(prompt('digite uma opçao:\n1 -soma\n2-subtraçao\n3-multiplicaçao\n4-divisao'))
+
+switch(opçao){
+    case 1:
+        let soma = algarismo1 + algarismo2
+        console.log(soma)
+        break;
+    case 2:
+         let subtraçao = algarismo1 - algarismo2
+        console.log(subtraçao)
+         break;
+    case 3:
+         let multiplicaçao = algarismo1 * algarismo2
+        console.log(multiplicaçao)
+        break;
+    case 4:
+        let divisao = algarismo1 / algarismo2
+        console.log(divisao)
+        break;
+
+            
+    
+    default:
+        console.log('opçao nao encontrada')
+}
